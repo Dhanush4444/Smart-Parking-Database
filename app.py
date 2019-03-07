76415da688c78bfa3d4cb4e1f90f1580209c52a5
Dhanush4444 <dhanushdazz@gmail.com>
2019-03-07 14:08:25 +0530

    Added functions to update spots

---
+++ app.py
@@ -0,0 +1,22 @@
+import requests as req
+
+#ONLY 6 PARKING SPOTS
+
+empty = 'EMPTY'
+occupied = 'OCCUPIED'
+
+url = 'https://smartparking00.herokuapp.com'
+
+def updateSpot(Spot,Status):
+    r = req.post(url + '/api/' + 'update/' + str(Spot) + '/' + Status)
+    d = r.text
+    print(d)
+
+def getJSON():
+    r = req.get(url + '/api/getjson')
+    d = r.json()
+    print(d)
+
+
+if __name__ == '__main__':
+    updateSpot(5,occupied)
